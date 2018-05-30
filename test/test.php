2cfd955d92e9df98a0363c054e5cf678bf898fa6
Herman Slatman <hermanslatman@hotmail.com>
2018-05-30 18:20:39 +0200

    Improve the example with some extra debugging parameters for SoapClient

--- test/test.php
+++ test/test.php
@@ -7,32 +7,51 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-use Slatman\EVerbinding;
-
-use Slatman\EVerbinding\EVerbindingClient;
-use Slatman\EVerbinding\EVerbindingClassmap;
-
+use Phpro\SoapClient\Soap\ClassMap\ClassMap;
 use Phpro\SoapClient\ClientFactory;
 use Phpro\SoapClient\ClientBuilder;
 use Symfony\Component\Console\Logger;
-
-use Phpro\SoapClient\Soap\ClassMap\ClassMap;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
+use Slatman\EVerbinding\EVerbindingClassmap;
+use Slatman\EVerbinding\EVerbindingClient;
+use Slatman\EVerbinding\Type\Login;
 
-$wsdl = 'testapi.everbinding.nl.v3.wsdl.xml';
-$clientFactory = new ClientFactory(EVerbindingClient::class);
-$soapOptions = [
-    'cache_wsdl' => WSDL_CACHE_NONE
+
+// Taken some Soap options from http://php.net/manual/en/class.soapclient.php#116724
+// Mainly the SSL stuff for debugging using a proxy with SSL termination.
+//
+// NOTE: Don't use the SSL options in production ever!
+//
+$stream_context_options = [
+    "ssl" => [
+        "verify_peer" => false,
+        "verify_peer_name" => false
+    ]
 ];
 
-$output = new \Symfony\Component\Console\Output\ConsoleOutput();
-$logger = new Logger\ConsoleLogger($output);
+$soap_options = [
+    'cache_wsdl' => WSDL_CACHE_NONE,
+    'proxy_port' => '8888',
+    'proxy_host' => 'localhost',
+    'stream_context' => stream_context_create($stream_context_options),
+    'verifypeer' => false,
+    'verifyhost' => false,
+];
 
+
+$output = new ConsoleOutput();
+$logger = new Logger\ConsoleLogger($output);
 $dispatcher = new EventDispatcher();
 
+$wsdl = 'testapi.everbinding.nl.v3.wsdl.xml'; // This can also be an URL
+
+/** @var ClientFactory $clientFactory */
+$clientFactory = new ClientFactory(EVerbindingClient::class);
+
 /** @var ClientBuilder $clientBuilder */
-$clientBuilder = new ClientBuilder($clientFactory, $wsdl, $soapOptions);
+$clientBuilder = new ClientBuilder($clientFactory, $wsdl, $soap_options);
 $clientBuilder->withLogger($logger);
 $clientBuilder->withEventDispatcher($dispatcher);
 $clientBuilder->addClassMap(new ClassMap('WsdlType', EVerbindingClassmap::class));
@@ -44,7 +63,8 @@ $client = $clientBuilder->build();
 $key = 'bla';
 $secret = 'bla';
 
-$login = new EVerbinding\Type\Login($key, $secret);
+/** @var Login $login */
+$login = new Login($key, $secret);
 $response = $client->login($login);
 
 echo $response->getSecurityToken();
